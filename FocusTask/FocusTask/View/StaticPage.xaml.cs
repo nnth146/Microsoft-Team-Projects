a7e4e80c5c5249c99f741a4aa1cc5b8688f9d8a8
Heroes Pluss <67665806+ha007duongviethung@users.noreply.github.com>
2022-05-19 07:49:19 +0700

    StaticPage - Focustask - FrontEnd - Completed

---
+++ FocusTask/FocusTask/View/StaticPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FocusTask.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StaticPage : Page
+    {
+        public StaticPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Repeat_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            List<string> list = new List<string>()
+            {
+                "Days",
+                "Weeks",
+                "Months",
+                "Years"
+            };
+            comboBox.ItemsSource = list;
+            comboBox.SelectedIndex = 1;
+        }
+
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            List<string> list = new List<string>();
+            for(int i  = 0; i <= 9999; i++)
+            {
+                list.Add(i.ToString());
+            }
+            comboBox.ItemsSource = list;
+            comboBox.SelectedIndex = 10;
+        }
+    }
+}
