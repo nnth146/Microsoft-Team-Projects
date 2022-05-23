0c640dc9533926de0e31888514903171a15c3ff4
nnth146 <nnthdeveloper@gmail.com>
2022-05-23 14:04:48 +0700

    Tạo project và thiết lập cơ bản

---
+++ KeepSafeForPassword/Uwp.Core/Uwp.Core/SupportResource/ResButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Uwp.Controls.SR
+{
+	public class ResButton : DependencyObject
+	{
+		public ResButton()
+		{
+			_resources = new ResourceDictionary();
+		}
+		private ResourceDictionary _resources;
+		public ResourceDictionary Resources
+		{
+			get
+			{
+				//Giúp cho tạo được nhiều resource
+				ResourceDictionary resource = new ResourceDictionary();
+				foreach (var key in _resources.Keys)
+				{
+					resource[key] = _resources[key];
+				}
+				return resource;
+			}
+		}
+		private const string _controlName = "Button";
+		public Brush BackgroundPointerOver
+		{
+			get => (Brush)_resources[_controlName + nameof(BackgroundPointerOver)];
+			set => _resources[_controlName + nameof(BackgroundPointerOver)] = value;
+		}
+		public Brush BackgroundPressed
+		{
+			get => (Brush)_resources[_controlName + nameof(BackgroundPressed)];
+			set => _resources[_controlName + nameof(BackgroundPressed)] = value;
+		}
+		public Brush BackgroundDisabled
+		{
+			get => (Brush)_resources[_controlName + nameof(BackgroundDisabled)];
+			set => _resources[_controlName + nameof(BackgroundDisabled)] = value;
+		}
+		public Brush ForegroundPointerOver
+		{
+			get => (Brush)_resources[_controlName + nameof(ForegroundPointerOver)];
+			set => _resources[_controlName + nameof(ForegroundPointerOver)] = value;
+		}
+		public Brush ForegroundPressed
+		{
+			get => (Brush)_resources[_controlName + nameof(ForegroundPressed)];
+			set => _resources[_controlName + nameof(ForegroundPressed)] = value;
+		}
+		public Brush ForegroundDisabled
+		{
+			get => (Brush)_resources[_controlName + nameof(ForegroundDisabled)];
+			set => _resources[_controlName + nameof(ForegroundDisabled)] = value;
+		}
+		public Brush BorderBrushPointerOver
+		{
+			get => (Brush)_resources[_controlName + nameof(BorderBrushPointerOver)];
+			set => _resources[_controlName + nameof(BorderBrushPointerOver)] = value;
+		}
+		public Brush BorderBrushPressed
+		{
+			get => (Brush)_resources[_controlName + nameof(BorderBrushPressed)];
+			set => _resources[_controlName + nameof(BorderBrushPressed)] = value;
+		}
+		public Brush BorderBrushDisabled
+		{
+			get => (Brush)_resources[_controlName + nameof(BorderBrushDisabled)];
+			set => _resources[_controlName + nameof(BorderBrushDisabled)] = value;
+		}
+		//GenerateAuto by tuanhungdev
+	}
+
+
+
+}
